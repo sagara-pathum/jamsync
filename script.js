b416183f6c22b2f4aebc38656b92e54ae9537a8d
sagara-pathum <sagarapathum0618@gmail.com>
2026-03-15 15:20:21 +0530

    Update WebSocket URL for signal server 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socketUrl = "https://webrtc-signal-server-9qhd.onrender.com"; // Update with your backend URL
+const socketUrl = "wss://jamsync-backend-q1ch.onrender.com"; // Update with your backend URL
 let socket;
 let localStream;
 let roomId;
